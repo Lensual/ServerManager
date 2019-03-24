e1dd7b16a7bb2950cdf4e74ecd58dfdcf91ee3bd
sena_yuki <wanglei199909210927@gmail.com>
2019-03-24 13:38:34 +0800

    新增DDNS

---
+++ module/ddns/__init__.py
@@ -0,0 +1,144 @@
+import logger
+import os
+import json
+import time
+from ddns.config import config
+
+def init():
+    logger.info("ddns inited")
+
+class DDNS_Interface():
+    supported=["Cloudflare"]
+    @staticmethod
+    def Cloudflare(Cloudflare_list,new_ipv4,new_ipv6):
+        for user in Cloudflare_list.keys():
+            user=Cloudflare_list[user]
+            cf=Cloudflare(user["email"],user["api_key"])
+            for domain in user["domains"].keys():
+                domain=user["domains"][domain]
+                if domain["type"]=="A":
+                    new_ip=new_ipv4
+                elif domain["type"]=="AAAA":
+                    new_ip=new_ipv6
+                if domain["enable"]==True:
+                    cf.update_DNS_Record(domain["prefix"],domain["domain"],new_ip,domain["type"])
+
+    
+
+class IPv6_Toolkit():
+    @staticmethod
+    def get_IPv6_Prefix_by_Route():#通过路由表获取前缀
+        getprefix=os.popen("route -A inet6 -n|grep ::/64|grep -v fe80::/64")
+        prefix=getprefix.read().split("/")[0]
+        logger.info("Get IPv6 prefix by route:"+prefix)
+        return prefix
+    @staticmethod
+    def get_IPv6_Addr_by_DefaultRoute():#通过默认路由获取/128地址
+        getip=os.popen("route -A inet6 -n|grep /128|grep -v fe80::|grep -v ::1/128")
+        ip=getip.read().split("/")[0]
+        logger.info("Get IPv6 address by route:"+ip)
+        return ip
+    @staticmethod
+    def get_IPv6_Addr_by_Site():
+        getip=os.popen("curl -k -s checkipv6.dyndns.com")
+        ip=getip.read().split(": ")[1].split("<")[0]
+        logger.info("Get IPv6 address by site:"+ip)
+        return ip
+
+class IPv4_Toolkit():
+    @staticmethod
+    def get_IPv4_Addr_by_ifconfig():
+        getip=os.popen("ifconfig |grep inet|grep -v inet6|grep -v 127.0.0.1")
+        ip=getip.read().split("inet")[1].split(" ")[1]
+        logger.info("Get IPv6 address by ifconfig:"+ip)
+        return ip
+    @staticmethod
+    def get_IPv4_Addr_by_Site_oray():
+        getip=os.popen("curl -k -s ddns.oray.com/checkip")
+        ip=getip.read().split("Current IP Address: ")[1].split("<")[0]
+        logger.info("Get IPv4 address by site:"+ip)
+        return ip
+
+    @staticmethod
+    def get_IPv4_Addr_by_Site_AWS():
+        getip=os.popen("curl -k -s http://checkip.amazonaws.com")
+        ip=getip.read()
+        logger.info("Get IPv4 ddress by site:"+ip)
+        return ip
+
+
+
+class Cloudflare():
+    
+    def __init__(self,email,key):
+        self.api_email=email
+        self.api_key=key
+        self.zone_id={}#zone_id={domain:id}
+        self.get_ZoneID()
+        self.record_id={}#record_id={domain:[{id,type,name,content}]}
+        self.get_RecordID()
+
+    def update_DNS_Record(self,prefix,domain,new_content,type):
+        if not (prefix=="@" or prefix==""):
+            name=prefix+"."+domain
+        else:
+            name=domain
+        zoneid=self.zone_id[domain]
+
+        recordid=""
+        for record in self.record_id[domain]:
+            if record["type"]==type and record["name"]==name:#如果找到已存在的匹配项
+                recordid=record["id"]
+                if record["content"]==new_content:#如果值没有变化
+                    logger.info(name+" IP no change")
+                    return
+                else:
+                    break
+        if recordid=="":#找不到则新建记录
+            logger.error("Can't find record!")
+            None
+        updaterecord=os.popen('''curl -k -s -X PUT "https://api.cloudflare.com/client/v4/zones/'''+zoneid+'''/dns_records/'''+recordid+'''" \
+            -H "X-Auth-Email: '''+self.api_email+'''" \
+            -H "X-Auth-Key: '''+self.api_key+'''" \
+            -H "Content-Type: application/json" \
+            --data "{\\"type\\":\\"'''+type+'''\\",\\"name\\":\\"'''+name+'''\\",\\"content\\":\\"'''+new_content+'''\\"}"
+        ''')
+        result=json.loads(updaterecord.read())
+        if result["success"]:
+            logger.info("Successfully Update DNS record of "+name+", from "+record["content"]+" to "+new_content)
+        else:
+            logger.error("Update DNS record of "+name+" failed! Error+"+result["errors"]["code"]+":"+result["errors"]["message"])
+        
+    #TODO 新建一条记录(type prefix,domain,content,ttl=120)
+
+    def get_ZoneID(self):
+        getzone=os.popen('''curl -k -s -X GET "https://api.cloudflare.com/client/v4/zones" \
+            -H "X-Auth-Email: '''+self.api_email+'''" \
+            -H "X-Auth-Key: '''+self.api_key+'''" \
+            -H "Content-Type: application/json"
+        ''')
+        domain_list=json.loads(getzone.read())["result"]
+        for domain in domain_list:
+            self.zone_id[domain["name"]]=domain["id"]
+
+    def get_RecordID(self):
+        for domain in self.zone_id.keys():
+            self.record_id[domain]=[]
+            getrecord=os.popen('''curl -k -s -X GET "https://api.cloudflare.com/client/v4/zones/'''+self.zone_id[domain]+'''/dns_records" \
+                -H "X-Auth-Email: '''+self.api_email+'''" \
+                -H "X-Auth-Key: '''+self.api_key+'''" \
+                -H "Content-Type: application/json"
+            ''')
+            record_list=json.loads(getrecord.read())["result"]
+            for record in record_list:
+                self.record_id[domain].append({"name":record["name"],"id":record["id"],"type":record["type"],"content":record["content"]})
+
+def start():
+    new_ipv4=IPv4_Toolkit.get_IPv4_Addr_by_Site_oray()
+    new_ipv6=IPv6_Toolkit.get_IPv6_Addr_by_Site()
+    for DDNS_SP in config.keys():
+        if DDNS_SP in DDNS_Interface.supported:
+            args=[config[DDNS_SP],new_ipv4,new_ipv6]
+            getattr(DDNS_Interface,DDNS_SP)(*args)
+
+start()
